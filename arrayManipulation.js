822ffcdad465c60f2f3613f22021e6793a8b5d48
Bransah <Emmanueladukantona9@gmail.com>
2024-05-19 20:07:09 +0000

    completion of task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,25 @@
-function processArray(arr) {}
+// Task One 
+function processArray(arr) {
+  
+  const processedArray = [];
+
+  
+  for (const num of arr) {
+  
+    if (num % 2 === 0) {
+      // Square the even number
+      processedArray.push(num * num);
+    } else {
+      // Triple the odd number
+      processedArray.push(num * 3);
+    }
+  }
+
+  return processedArray;
+}
+
+const originalArray = [1, 4, 3, 6, 7, 8];
+const processedArray = processArray(originalArray);
+
+console.log("Original Array:", originalArray);
+console.log("Processed Array:", processedArray);
